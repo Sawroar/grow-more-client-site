455c6a75e8e4da0dd26025dba6cca8157dc091d2
sawroar <sawroarhossain5@gmail.com>
2022-10-28 03:08:36 +0600

    Footer Section added

--- src/Shared/Footer.js
+++ src/Shared/Footer.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { FaUser, } from 'react-icons/fa';
+import { FaUser, FaFacebook, FaInstagram } from 'react-icons/fa';
 const Footer = () => {
     return (
-        <div className='bg-dark mt-5 mb-5'>
-            <h6 className='text-white text-center py-2 mb-5'>CopyRight @ Grow More Inc</h6>
-            <FaUser></FaUser>
-            <div>
-                <span className="footer-title">Services</span>
-
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
+        <div className='bg-dark mt-5 mb-5 p-2'>
+            <div className=''>
+                <div >
+                    <h6 className='text-white text-center py-2 mb-2'>CopyRight @ Grow More Inc</h6></div>
+                <div className='text-white text-center'>
+                    <FaUser className='rounded ms-2' ></FaUser>
+                    <FaFacebook className='rounded ms-2' ></FaFacebook>
+                    <FaInstagram className='rounded ms-2'></FaInstagram>
+                </div>
+            </div>
+            <div className=' text-center p-5'>
+                <a className="link link-hover  ms-2">©2022 All rights reserved</a>
+                <a className="link link-hover  ms-2">Privacy policy</a>
+                <a className="link link-hover  ms-2 ">Terms of service</a>
             </div>
 
 
